64982d5cf9c4491e5bd87275f445b178de9beef9
Venkata Sai Kumar Gottumukkala <83054740+Venkata-sai-kumar@users.noreply.github.com>
2026-06-12 11:58:56 -0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        sdict = {}
+        tdict = {}
+        for i in range(len(s)):
+            sdict[s[i]] = 1 + sdict.get(s[i], 0)
+            tdict[t[i]] = 1 + tdict.get(t[i], 0)
+        return sdict == tdict
+        # for txt in s:
+        #     if not txt in sdict:
+        #         sdict[txt] = 1
+        #     else:
+        #         sdict[txt] += 1
+        # for txt in t:
+        #     if txt not in tdict:
+        #         tdict[txt] = 1
+        #     else:
+        #         tdict[txt] += 1
+        # if len(s) > len(t): 
+        #     for key, value in sdict.items():
+        #         if key in tdict and tdict[key] == value:
+        #             pass
+        #         elif not key in tdict:
+        #             return False
+        #         elif tdict[key] != value:
+        #             return False
+        # else:
+        #     for key, value in tdict.items():
+        #         if key in sdict and sdict[key] == value:
+        #             pass
+        #         elif not key in sdict:
+        #             return False
+        #         elif sdict[key] != value:
+        #             return False
+        # return True
